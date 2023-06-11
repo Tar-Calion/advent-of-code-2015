5100f7b6c4f39d55fb4137cb4eb9cbbbd2d48c72
Tar-Calion <43185234+Tar-Calion@users.noreply.github.com>
2023-06-11 22:29:30 +0200

    check for undefined and null values

--- __tests__/server/day1.test.js
+++ __tests__/server/day1.test.js
@@ -1,13 +1,9 @@
 import {puzzle1} from "../../src/server/day1.js";
 
 describe('day1', () => {
-    it('should return 0 if input is empty', function () {
-        const input = '';
-        const answer = puzzle1(input);
-        expect(answer).toEqual(0);
-    });
-    
     it.each([
+        [null, 0],
+        [undefined, 0],
         ['', 0],
         ['(', 1],
         [')', -1],
@@ -21,6 +17,7 @@ describe('day1', () => {
         ['))(', -1],
         [')))', -3],
         [')())())', -3],
+        ['(abc)())!!!', -1],
     ])('should compute floor for input %s', (input, expected) => {
         const answer = puzzle1(input);
         expect(answer).toEqual(expected);
--- src/server/day1.js
+++ src/server/day1.js
@@ -1,6 +1,9 @@
 
 function puzzle1(input) {
     
+    if (input == null) {
+        return 0;
+    }
     let floor = 0;
     for (let i = 0; i < input.length; i++) {
         const c = input.charAt(i);
